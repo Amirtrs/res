4d86f9cfc8d997bbfb7e8d19d02922ec37a8a665
amirtrs <amir.toranj21@gmail.com>
2020-04-28 19:42:44 +0430

    Updates

---
+++ precache-manifest.7eb0f3bb456abfe5d284050c80ad2ef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d246b86514f68f496980a8fb574c004",
+    "url": "/res/index.html"
+  },
+  {
+    "revision": "611981358fe43a8ab641",
+    "url": "/res/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "23886f38a9290d03c624",
+    "url": "/res/static/js/2.3812fcdb.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/res/static/js/2.3812fcdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611981358fe43a8ab641",
+    "url": "/res/static/js/main.1f4071d6.chunk.js"
+  },
+  {
+    "revision": "6f94e42b64c63276d903",
+    "url": "/res/static/js/runtime-main.5e8a284b.js"
+  }
+]);
